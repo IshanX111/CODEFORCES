a6752484f9cdb0f056b84c93d63e05fc14a64643
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2020-04-09 06:11:15 +0600

    Add files via upload

---
+++ (A) Bear and Big Brother.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin>>a>>b;
+    c=0;
+    while(a<=b){
+        a=a*3;
+        b=b*2;
+        c++;
+
+    }
+    cout<<c<<endl;
+
+
+
+}
