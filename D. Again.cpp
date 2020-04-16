c5540b90791af5677e6fdce4f8673bef28e651e4
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2020-04-16 17:53:14 +0600

    Add files via upload

---
+++ D. Again.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    string s;
+    cin>>s;
+    int x=s.at(6)-48;
+    if(x%2==0)
+    cout<<"0"<<endl;
+    else{
+        cout<<"1"<<endl;
+    }
+
+}
