ee581e4d1270999d3caffdc6c995225d9edb180e
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2020-05-22 06:32:28 +0600

    Add files via upload

---
+++ A.a+b trial problem.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define PI acos(-1.0)
+#define rep1(i,n) for(i=1;i<n;i++)
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep(i,a,n) for(i=a;i<n;i++)
+#define repe1(i,n) for(i=1;i<=n;i++)
+#define repe0(i,n) for(i=0;i<=n;i++)
+#define repe(i,a,n) for(i=a;i<=n;i++)
+#define fr freopen("input.txt","r",stdin)
+#define fw freopen("output.txt","w",stdout)
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+typedef long long  ll;
+using namespace std;
+int main(){
+
+    ll t,a,b;
+    cin>>t;
+    while(t--){
+
+    cin>>a>>b;
+    cout<<a+b<<endl;
+
+    }
+}
+
