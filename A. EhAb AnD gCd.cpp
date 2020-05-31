ffec45e220ebce9a2c3eba2900b528140f13ef75
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2020-05-31 06:13:28 +0600

    Add files via upload

---
+++ A. EhAb AnD gCd.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    long long t;
+    cin>>t;
+
+    while(t--){
+        long long n;
+        cin>>n;
+        cout<<1<<" "<<n-1<<endl;
+
+    }
+
+}
