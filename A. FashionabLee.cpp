517f94efb573e3c41987908082c72bdae13fb784
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2020-06-24 19:46:42 +0600

    Add files via upload

---
+++ A. FashionabLee.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    ll n,t;
+    cin>>t;
+    while(t--){
+
+        cin>>n;
+        if(n>2 && n%4==0){
+                cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+
+
+    }
+
+
+}
