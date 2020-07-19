512928e8b867730058b4aac0dfafc211815f867d
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2020-07-19 14:51:07 +0600

    Add files via upload

---
+++ A. The number of positions.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int a,b,c,x;
+    cin>>a>>b>>c;
+    x=a-b;
+    if(x>c){
+        cout<<c+1<<endl;
+    }
+    else{
+
+        cout<<a-b<<endl;
+
+    }
+
+
+}
